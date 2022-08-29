a0172f753897a6a2c5120b4e26545828841ac7b8
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-08-29 14:32:10 +0100

    Add files via upload

---
+++ mods/improved-interface-options.lua
@@ -0,0 +1,27 @@
+-- Based on zUI skinning (https://github.com/Ko0z/zUI)
+-- Credit to Ko0z (https://github.com/Ko0z/)
+
+local module = ShaguTweaks:register({
+    title = "Improved Interface Options",
+    description = "Removes the background while in the Interface Options",
+    expansions = { ["vanilla"] = true, ["tbc"] = nil },
+    category = nil,
+    enabled = nil,
+  })
+  
+module.enable = function(self)
+
+    UIOptionsFrame:SetScript("OnShow", function()
+        -- default events
+        UIOptionsFrame_Load();
+        MultiActionBar_Update();
+        MultiActionBar_ShowAllGrids();
+        Disable_BagButtons();
+        UpdateMicroButtons();
+
+        -- customize
+        UIOptionsBlackground:Hide()
+        UIOptionsFrame:SetScale(.8)
+    end)
+
+end
