2ab41bf90331e8b0b9c9f8c4267975a60b649a43
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-08-30 15:59:18 +0100

    Add files via upload

--- mods/central-ui.lua
+++ mods/central-ui.lua
@@ -6,75 +6,73 @@ local module = ShaguTweaks:register({
     enabled = nil,
   })
   
-  module.enable = function(self)
+local function unitframes()    
+    --[[ When scaling, the unit frame may move offscreen ]]
 
-    local function unitframes()    
-        --[[ When scaling, the unit frame may move offscreen ]]
-    
-        -- Player / Target
-        local scale = 1    
-        local xoffset = 20
-        local yoffset = -150
-    
-        -- Player
-        PlayerFrame:ClearAllPoints()
-        PlayerFrame:SetScale(scale)
-        PlayerFrame:SetPoint("RIGHT", UIParent, "CENTER", -xoffset, yoffset)
-        
-        -- Target
-        TargetFrame:ClearAllPoints()
-        TargetFrame:SetScale(scale)
-        TargetFrame:SetPoint("LEFT", UIParent, "CENTER", xoffset, yoffset)
-        
-        -- Party
-        local scale = 1.4
-        PartyMemberFrame1:ClearAllPoints()
-        PartyMemberFrame1:SetScale(scale)
-        PartyMemberFrame2:SetScale(scale)
-        PartyMemberFrame3:SetScale(scale)
-        PartyMemberFrame4:SetScale(scale)
-        PartyMemberFrame1:SetPoint("RIGHT", UIParent, "CENTER", -200, 100)
-        -- PartyMemberFrame2/3/4 moves with PartyMemberFrame1
+    -- Player / Target
+    local scale = 1    
+    local x = 20
+    local y = -150
+
+    -- Player
+    PlayerFrame:ClearAllPoints()
+    PlayerFrame:SetScale(scale)
+    PlayerFrame:SetPoint("RIGHT", UIParent, "CENTER", -x, y)
+
+    -- Target
+    TargetFrame:ClearAllPoints()
+    TargetFrame:SetScale(scale)
+    TargetFrame:SetPoint("LEFT", UIParent, "CENTER", x, y)
+
+    -- Party
+    local scale = 1.4
+    PartyMemberFrame1:ClearAllPoints()
+    PartyMemberFrame1:SetScale(scale)
+    PartyMemberFrame2:SetScale(scale)
+    PartyMemberFrame3:SetScale(scale)
+    PartyMemberFrame4:SetScale(scale)
+    PartyMemberFrame1:SetPoint("RIGHT", UIParent, "CENTER", -200, 100)
+    -- PartyMemberFrame2/3/4 moves with PartyMemberFrame1
+end
+
+local function castbar()
+    if IsAddOnLoaded("GryllsSwingTimer") then
+        CastingBarFrame:SetPoint("TOP", SP_ST_Frame, "BOTTOM", 0, -14)
+    else
+        CastingBarFrame:SetPoint("CENTER", UIParent, "CENTER", 0, -214)
     end
-    
-    local function castbar()
-        if IsAddOnLoaded("GryllsSwingTimer") then
-            CastingBarFrame:SetPoint("TOP", SP_ST_Frame, "BOTTOM", 0, -14)
-        else
-            CastingBarFrame:SetPoint("CENTER", UIParent, "CENTER", 0, -214)
-        end
-    end   
-    
-    local function minimap()
-        -- local x = 448
-        local x = (MultiBarLeft:GetWidth()*2)+400+MultiBarLeft:GetWidth()
-        local y = 10
-        MinimapCluster:ClearAllPoints()
-        -- MinimapCluster:SetPoint("TOPLEFT", UIParent, x, 0)
-        MinimapCluster:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -x, -y)
-    end
-    
-    local function buffs()
-        -- Buffs start with TemporaryEnchantFrame
-        -- Debuffs are aligned underneath the TemporaryEnchantFrame    
-        TemporaryEnchantFrame:ClearAllPoints()
-        TemporaryEnchantFrame:SetPoint("TOPLEFT", MinimapCluster, -15, -28)
+end   
 
-        -- prevent TemporaryEnchantFrame from moving
-        TemporaryEnchantFrame.ClearAllPoints = function() end
-        TemporaryEnchantFrame.SetPoint = function() end
-    end
+local function minimap()
+    -- local x = 448
+    local x = (MultiBarLeft:GetWidth()*2)+400+MultiBarLeft:GetWidth()
+    local y = 10
+    MinimapCluster:ClearAllPoints()
+    -- MinimapCluster:SetPoint("TOPLEFT", UIParent, x, 0)
+    MinimapCluster:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -x, -y)
+end
 
-    local moveFrame = CreateFrame("Frame", nil, UIParent)	
-    moveFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+local function buffs()
+    -- Buffs start with TemporaryEnchantFrame
+    -- Debuffs are aligned underneath the TemporaryEnchantFrame    
+    TemporaryEnchantFrame:ClearAllPoints()
+    TemporaryEnchantFrame:SetPoint("TOPLEFT", MinimapCluster, -15, -28)
 
-    moveFrame:SetScript("OnEvent", function()
-        if event == "PLAYER_ENTERING_WORLD" then
-            unitframes()
-            castbar()         
-            minimap()
-            buffs()
-        end
+    -- prevent TemporaryEnchantFrame from moving
+    TemporaryEnchantFrame.ClearAllPoints = function() end
+    TemporaryEnchantFrame.SetPoint = function() end
+end
+
+module.enable = function(self)
+    
+    local events = CreateFrame("Frame", nil, UIParent)	
+    events:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+    events:SetScript("OnEvent", function()
+        unitframes()
+        castbar()         
+        minimap()
+        buffs()
     end)
 
 end
