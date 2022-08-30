b0940f891d66c1e9a544639e216a229cd8db5860
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-08-30 16:07:43 +0100

    Add files via upload

--- mods/hide-combat-tooltip.lua
+++ mods/hide-combat-tooltip.lua
@@ -1,51 +1,49 @@
 local module = ShaguTweaks:register({
   title = "Hide Combat Tooltip",
   description = "Hides the tooltip while in combat. While in combat, holding shift while mousing over a new target will show the tooltip.",
-  expansions = { ["vanilla"] = true, ["tbc"] = true },
+  expansions = { ["vanilla"] = true, ["tbc"] = nil },
   category = "Tooltip & Items",
   enabled = nil,
 })
 
-module.enable = function(self)    
-    
-    local combatFrame = CreateFrame("Frame", nil, UIParent)	
-    combatFrame:RegisterEvent("PLAYER_REGEN_ENABLED") -- out of combat
-    combatFrame:RegisterEvent("PLAYER_REGEN_DISABLED") -- in combat
+local events = CreateFrame("Frame", nil, UIParent)	
+    events:RegisterEvent("PLAYER_REGEN_ENABLED") -- out of combat
+    events:RegisterEvent("PLAYER_REGEN_DISABLED") -- in combat
 
-    local inCombat = false
+local inCombat = false
 
-    local function tooltipToggle(inCombat)
-        if inCombat then
-            local modifierDown = IsShiftKeyDown() -- IsControlKeyDown() or IsAltKeyDown() can be used instead
-            if modifierDown then
-                
-                GameTooltip:Show()
-            else
-                GameTooltip:Hide()
-            end
-        else
+local function tooltipToggle(inCombat)
+    if inCombat then
+        local modifierDown = IsShiftKeyDown() -- IsControlKeyDown() or IsAltKeyDown() can be used instead
+        if modifierDown then            
             GameTooltip:Show()
-        end
-    end
-
-    local function tooltipSetScript(inCombat)
-        if inCombat then
-            combatFrame:SetScript("OnUpdate", function()
-                tooltipToggle(inCombat)
-            end)
         else
-            combatFrame:SetScript("OnUpdate", nil)
+            GameTooltip:Hide()
         end
+    else
+        GameTooltip:Show()
     end
+end
 
-    combatFrame:SetScript("OnEvent", function()
+local function tooltipSetScript(inCombat)
+    if inCombat then
+        events:SetScript("OnUpdate", function()
+            tooltipToggle(inCombat)
+        end)
+    else
+        events:SetScript("OnUpdate", nil)
+    end
+end
+
+module.enable = function(self)
+
+    events:SetScript("OnEvent", function()
         if event == "PLAYER_REGEN_DISABLED" then
-            inCombat = true
-            tooltipSetScript(inCombat)
+            inCombat = true            
         elseif event == "PLAYER_REGEN_ENABLED" then
             inCombat = false
-            tooltipSetScript(inCombat)
         end
+        tooltipSetScript(inCombat)
     end)
 
 end
