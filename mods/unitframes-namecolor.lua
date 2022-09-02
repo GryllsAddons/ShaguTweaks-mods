c3dd619d4f566d36a6560bdf5d80c91ada8c9187
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-09-03 00:51:05 +0100

    Add files via upload

--- mods/unitframes-namecolor.lua
+++ mods/unitframes-namecolor.lua
@@ -6,87 +6,86 @@ local module = ShaguTweaks:register({
   enabled = nil,
 })
 
-local function colorName(unit)
-    local name
-    if unit == "player" then
-        name = PlayerFrame.name
-    elseif unit == "pet" then
-        name = PetName
-    elseif unit == "target" then
-        name = TargetFrame.name
-    elseif unit == "targettarget" then
-        name = TargetofTargetName
-    elseif unit == "party1" then
-        name = PartyMemberFrame1.name
-    elseif unit == "party2" then
-        name = PartyMemberFrame2.name
-    elseif unit == "party3" then
-        name = PartyMemberFrame3.name
-    elseif unit == "party4" then
-        name = PartyMemberFrame4.name
-    elseif unit == "partypet1" then
-        name = PartyMemberFrame1PetFrame.name
-    elseif unit == "partypet2" then
-        name = PartyMemberFrame2PetFrame.name
-    elseif unit == "partypet3" then
-        name = PartyMemberFrame3PetFrame.name
-    elseif unit == "partypet4" then
-        name = PartyMemberFrame4PetFrame.name
-    end
-
-    if name then       
-        local r,g,b
-        if UnitIsPlayer(unit) then
-            -- color by class
-            local _, class = UnitClass(unit)
-            if class == "SHAMAN" then
-                r, g, b = 0/255, 112/255, 221/255 -- blue shamans
-            else
-                local color = RAID_CLASS_COLORS[class]
-                r,g,b = color.r, color.g, color.b
-            end
-        else
-            -- not a player
-            local creatureType = UnitCreatureType(unit)
-            local friend = UnitIsFriend("player", unit)
-            if friend then
-                if creatureType == "Demon" then -- friendly Warlock pet
-                    r,g,b = 148/255, 130/255, 201/255
-                elseif creatureType == "Beast" then -- friendly Hunter pet
-                    r,g,b = 171/255, 212/255, 115/255
-                else
-                    r,g,b = 255/255, 210/255, 0/255  -- default color
-                end
-            else
-                -- enemy
-                -- color name by reaction
-                r,g,b = GameTooltip_UnitColor(unit)
-            end
-        end
-
-        name:SetTextColor(r,g,b)
-    end
-end
-
-local targeting = CreateFrame("Frame", nil, UIParent)
-local name
-
-local function colorToT()
-    if UnitExists("target") then        
-        targeting:SetScript("OnUpdate", function()
-            if UnitExists("targettarget") then        
-                if GetUnitName("targettarget") ~= name then
-                    name = GetUnitName("targettarget")
-                    colorName("targettarget")
-                end
-            end
-        end)
-    else
-        targeting:SetScript("OnUpdate", nil)
-    end
-end
-
 module.enable = function(self)
+    local function colorName(unit)
+        local name
+        if unit == "player" then
+            name = PlayerFrame.name
+        elseif unit == "pet" then
+            name = PetName
+        elseif unit == "target" then
+            name = TargetFrame.name
+        elseif unit == "targettarget" then
+            name = TargetofTargetName
+        elseif unit == "party1" then
+            name = PartyMemberFrame1.name
+        elseif unit == "party2" then
+            name = PartyMemberFrame2.name
+        elseif unit == "party3" then
+            name = PartyMemberFrame3.name
+        elseif unit == "party4" then
+            name = PartyMemberFrame4.name
+        elseif unit == "partypet1" then
+            name = PartyMemberFrame1PetFrame.name
+        elseif unit == "partypet2" then
+            name = PartyMemberFrame2PetFrame.name
+        elseif unit == "partypet3" then
+            name = PartyMemberFrame3PetFrame.name
+        elseif unit == "partypet4" then
+            name = PartyMemberFrame4PetFrame.name
+        end
+    
+        if name then       
+            local r,g,b
+            if UnitIsPlayer(unit) then
+                -- color by class
+                local _, class = UnitClass(unit)
+                if class == "SHAMAN" then
+                    r, g, b = 0/255, 112/255, 221/255 -- blue shamans
+                else
+                    local color = RAID_CLASS_COLORS[class]
+                    r,g,b = color.r, color.g, color.b
+                end
+            else
+                -- not a player
+                local creatureType = UnitCreatureType(unit)
+                local friend = UnitIsFriend("player", unit)
+                if friend then
+                    if creatureType == "Demon" then -- friendly Warlock pet
+                        r,g,b = 148/255, 130/255, 201/255
+                    elseif creatureType == "Beast" then -- friendly Hunter pet
+                        r,g,b = 171/255, 212/255, 115/255
+                    else
+                        r,g,b = 255/255, 210/255, 0/255  -- default color
+                    end
+                else
+                    -- enemy
+                    -- color name by reaction
+                    r,g,b = GameTooltip_UnitColor(unit)
+                end
+            end
+    
+            name:SetTextColor(r,g,b)
+        end
+    end
+    
+    local targeting = CreateFrame("Frame", nil, UIParent)
+    local name
+    
+    local function colorToT()
+        if UnitExists("target") then        
+            targeting:SetScript("OnUpdate", function()
+                if UnitExists("targettarget") then        
+                    if GetUnitName("targettarget") ~= name then
+                        name = GetUnitName("targettarget")
+                        colorName("targettarget")
+                    end
+                end
+            end)
+        else
+            targeting:SetScript("OnUpdate", nil)
+        end
+    end
 
     local events = CreateFrame("Frame", nil, UIParent)
     events:RegisterEvent("PLAYER_ENTERING_WORLD")    
@@ -126,5 +125,4 @@ module.enable = function(self)
             end
         end
     end)
-
 end
