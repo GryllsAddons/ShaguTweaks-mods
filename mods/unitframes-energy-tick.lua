736786a5d36066350d6832cf96fe7e4501ba16f0
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-09-06 00:41:39 +0100

    Add files via upload

--- mods/unitframes-energy-tick.lua
+++ mods/unitframes-energy-tick.lua
@@ -14,51 +14,47 @@ module.enable = function(self)
     energytick:RegisterEvent("UNIT_ENERGY")
     energytick:RegisterEvent("UNIT_MANA")
     energytick:SetScript("OnEvent", function()
-    if UnitPowerType("player") == 0 then
-      this.mode = "MANA"
-      this:Show()
-    elseif UnitPowerType("player") == 3 then
-      this.mode = "ENERGY"
-      this:Show()
-    else
-      this:Hide()
-    end
-
-    if event == "PLAYER_ENTERING_WORLD" then
-      this.lastMana = UnitMana("player")
-      this.spark:SetVertexColor(1, 1, 1, 1)
-    end
-
-    if (event == "UNIT_MANA" or event == "UNIT_ENERGY") and arg1 == "player" then
-      this.currentMana = UnitMana("player")
-      local diff = 0
-      if this.lastMana then
-        diff = this.currentMana - this.lastMana
-      end
-
-      if this.mode == "MANA" and diff < 0 then
-        this.target = 5
-      elseif this.mode == "MANA" and diff > 0 then
-        if this.max ~= 5 and diff > (this.badtick and this.badtick*1.2 or 5) then
-          this.target = 2
+      if UnitPowerType("player") == 0 then
+        this.mode = "MANA"
+        -- hide if full mana and not in combat
+        if (UnitMana("player") == UnitManaMax("player")) and (not UnitAffectingCombat("player")) then
+          this:Hide()
         else
-          this.badtick = diff
+          this:Show()
         end
-      elseif this.mode == "ENERGY" and diff > 0 then
-        this.target = 2
+      elseif UnitPowerType("player") == 3 then
+        this.mode = "ENERGY"
+        this:Show()
+      else
+        this:Hide()
       end
-      this.lastMana = this.currentMana
 
-      -- hide if full mana and not in combat
-      if this.mode == "MANA" then
-          if (UnitMana("player") == UnitManaMax("player")) and (not UnitAffectingCombat("player")) then
-            energytick:Hide()
-          else
-            energytick:Show()
-          end
+      if event == "PLAYER_ENTERING_WORLD" then
+        this.lastMana = UnitMana("player")
+        this.spark:SetVertexColor(1, 1, 1, 1)
       end
-    end
-  end)
+
+      if (event == "UNIT_MANA" or event == "UNIT_ENERGY") and arg1 == "player" then
+        this.currentMana = UnitMana("player")
+        local diff = 0
+        if this.lastMana then
+          diff = this.currentMana - this.lastMana
+        end
+
+        if this.mode == "MANA" and diff < 0 then
+          this.target = 5
+        elseif this.mode == "MANA" and diff > 0 then
+          if this.max ~= 5 and diff > (this.badtick and this.badtick*1.2 or 5) then
+            this.target = 2
+          else
+            this.badtick = diff
+          end
+        elseif this.mode == "ENERGY" and diff > 0 then
+          this.target = 2
+        end
+        this.lastMana = this.currentMana      
+      end
+    end)
   
   local pheight, pwidth = PlayerFrameManaBar:GetHeight(), PlayerFrameManaBar:GetWidth()
   energytick:SetScript("OnUpdate", function()    
