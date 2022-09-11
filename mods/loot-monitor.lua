56614e67a245c7ce9de960bad544175edac8dd7b
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-09-11 01:38:55 +0100

    Add files via upload

--- mods/loot-monitor.lua
+++ mods/loot-monitor.lua
@@ -9,7 +9,7 @@ local module = ShaguTweaks:register({
 module.enable = function(self)
     local loot = CreateFrame("ScrollingMessageFrame")
     loot:SetPoint("TOP", UIErrorsFrame, "BOTTOM", 0, 0)
-    loot:SetWidth(200)
+    loot:SetWidth(400)
     loot:SetHeight(70) -- 5 lines
     
     -- loot:SetFontObject(GameTooltipTextSmall)
