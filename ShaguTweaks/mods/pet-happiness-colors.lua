226ad7aaaff42f3eeb4fea31752c155929f5a447
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-11-20 19:21:27 +0000

    Add files via upload

--- ShaguTweaks/mods/pet-happiness-colors.lua
+++ ShaguTweaks/mods/pet-happiness-colors.lua
@@ -1,8 +1,3 @@
-local _G = _G or getfenv(0)
-local Abbreviate = ShaguTweaks.Abbreviate
-local GetColorGradient = ShaguTweaks.GetColorGradient
-local vanilla = ShaguTweaks.GetExpansion() == "vanilla" or nil
-
 local module = ShaguTweaks:register({
   title = "Pet Happiness Colors",
   description = "Colors Hunter pet healthbar by happiness.",
@@ -12,30 +7,25 @@ local module = ShaguTweaks:register({
 })
 
 module.enable = function(self)
-  local function happiness()
-    local _, class = UnitClass("player")
-    if not (class == "HUNTER") then return end
+  local _, class = UnitClass("player")
+  if not (class == "HUNTER") then return end
 
-    local function color(unit)
-      if (not UnitExists(unit)) or UnitIsDead(unit) or UnitIsGhost(unit) then return end
-        -- credit to KoOz (https://github.com/Ko0z/UnitFramesImproved_Vanilla)
-        local happiness, damagePercentage, loyaltyRate = GetPetHappiness()
-        if (happiness == 3) then
-          PetFrameHealthBar:SetStatusBarColor(0,1,0)
-        elseif (happiness == 2) then
-          PetFrameHealthBar:SetStatusBarColor(1,1,0)
-        else
-          PetFrameHealthBar:SetStatusBarColor(1,0,0)
-        end
+  local function color(unit)
+    if (not UnitExists(unit)) or UnitIsDead(unit) or UnitIsGhost(unit) then return end
+    -- credit to KoOz (https://github.com/Ko0z/UnitFramesImproved_Vanilla)
+    local happiness = GetPetHappiness()
+    if (happiness == 3) then
+      PetFrameHealthBar:SetStatusBarColor(0,1,0)
+    elseif (happiness == 2) then
+      PetFrameHealthBar:SetStatusBarColor(1,1,0)
+    else
+      PetFrameHealthBar:SetStatusBarColor(1,0,0)
     end
-
-    local events = CreateFrame("Frame", nil, UIParent)
-    events:RegisterEvent("UNIT_HEALTH", "player")
-
-    events:SetScript("OnEvent", function()
-      color(arg1)
-    end)
   end
 
-  happiness()
+  local events = CreateFrame("Frame", nil, UIParent)
+  events:RegisterEvent("UNIT_PET", "player")
+  events:SetScript("OnEvent", function()
+    color("pet")
+  end)
 end
