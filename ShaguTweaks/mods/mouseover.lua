7cc3c78601253cd0d5b049e42912ac278fb3cc48
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-04-21 14:17:58 +0100

    hcwarn check - disable in instances

--- ShaguTweaks/mods/mouseover.lua
+++ ShaguTweaks/mods/mouseover.lua
@@ -70,7 +70,7 @@ module.enable = function(self)
 
     -- HCWarn support
     if HCWarn_nointeract and (unit ~= "player") then
-      if UnitIsPVP(unit) then 
+      if UnitIsPVP(unit) and (not IsInInstance()) then 
           UIErrorsFrame:AddMessage("Mouseover unit is PVP flagged", 1, 0.25, 0)
         return 
       end
