cf7afb63667fc4272e547eb72b296360c4394bbb
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-05-17 16:57:24 +0100

    change quality in instances

--- mods/max-graphics.lua
+++ mods/max-graphics.lua
@@ -7,13 +7,14 @@ local module = ShaguTweaks:register({
 })
   
 module.enable = function(self)
+    -- https://docs.google.com/spreadsheets/d/17bXs9WhOkP8Zknl1GYXCFVdHYOdgxoRFrIe7Os3BtRo/edit#gid=0
     -- https://forum.nostalrius.org/viewtopic.php?t=1100
     SlashCmdList["CONSOLE"]("anisotropic 16")
     SlashCmdList["CONSOLE"]("baseMip 0")
     SlashCmdList["CONSOLE"]("detailDoodadAlpha 100")
-    SlashCmdList["CONSOLE"]("DistCull 888") -- 888 / 500
+    -- SlashCmdList["CONSOLE"]("DistCull 888") -- 500 / 888
     SlashCmdList["CONSOLE"]("doodadAnim 1")
-    SlashCmdList["CONSOLE"]("farclip 777")
+    -- SlashCmdList["CONSOLE"]("farclip 777") -- 500 / 777
     SlashCmdList["CONSOLE"]("ffx 1")
     SlashCmdList["CONSOLE"]("ffxDeath 1")
     SlashCmdList["CONSOLE"]("ffxGlow 1")
@@ -24,7 +25,7 @@ module.enable = function(self)
     SlashCmdList["CONSOLE"]("gxDepthBits 24")    
     SlashCmdList["CONSOLE"]("horizonfarclip 2112")
     SlashCmdList["CONSOLE"]("lod 0")
-    SlashCmdList["CONSOLE"]("lodDist 250")
+    -- SlashCmdList["CONSOLE"]("lodDist 250") -- 100 / 250
     SlashCmdList["CONSOLE"]("mapObjLightLOD 2")
     SlashCmdList["CONSOLE"]("mapObjOverbright 1")
     SlashCmdList["CONSOLE"]("mapShadows 1")
@@ -47,7 +48,7 @@ module.enable = function(self)
     SlashCmdList["CONSOLE"]("texLodBias -1")
     SlashCmdList["CONSOLE"]("textureLodDist 777")
     SlashCmdList["CONSOLE"]("trilinear 1")
-    SlashCmdList["CONSOLE"]("unitDrawDist 1000") -- 1000 / 300
+    SlashCmdList["CONSOLE"]("unitDrawDist 300")
     SlashCmdList["CONSOLE"]("waterLOD 0")
     SlashCmdList["CONSOLE"]("waterParticulates 1")
     SlashCmdList["CONSOLE"]("waterRipples 1")
@@ -57,4 +58,38 @@ module.enable = function(self)
     SlashCmdList["CONSOLE"]("gxMultisample 8") -- (1 is no anti-alising, 2 is 2x, 4 is 4x, 8 is 8x)
     SlashCmdList["CONSOLE"]("gxMultisampleQuality 1")
     SlashCmdList["CONSOLE"]("showCull")
+
+    local graphics = CreateFrame("Frame", nil, UIParent)
+
+    function graphics:performance()
+        SlashCmdList["CONSOLE"]("DistCull 500") -- 500 / 888
+        SlashCmdList["CONSOLE"]("farclip 500") -- 500 / 777
+        SlashCmdList["CONSOLE"]("lodDist 100") -- 100 / 250
+    end
+
+    function graphics:quality()
+        SlashCmdList["CONSOLE"]("DistCull 888") -- 500 / 888
+        SlashCmdList["CONSOLE"]("farclip 777") -- 500 / 777
+        SlashCmdList["CONSOLE"]("lodDist 250") -- 100 / 250        
+    end
+
+    function graphics:mapUpdate()
+        local zone = GetRealZoneText()
+        if graphics.zone ~= zone then
+            graphics.zone = zone
+            if IsInInstance() then
+                graphics:performance()                
+            else
+                graphics:quality()               
+            end
+        end
+    end
+
+    graphics:RegisterEvent("PLAYER_ENTERING_WORLD")
+    graphics:RegisterEvent("MINIMAP_ZONE_CHANGED")
+    graphics:SetScript("OnEvent", function()
+        if event == "MINIMAP_ZONE_CHANGED" then
+            graphics:mapUpdate()
+        end
+    end)
 end
