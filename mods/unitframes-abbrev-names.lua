677f0b1071a624de1816e4b953f658712e1305a1
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-06-11 11:12:37 +0100

    Add files via upload

---
+++ mods/unitframes-abbrev-names.lua
@@ -0,0 +1,48 @@
+local module = ShaguTweaks:register({
+    title = "Unit Frame Abbreviated Names",
+    description = "Abbreviates long unit names.",
+    expansions = { ["vanilla"] = true, ["tbc"] = nil },
+    category = "Unit Frames",
+    enabled = nil,
+})
+
+module.enable = function(self)
+    local function abbrevname(t)
+        return string.sub(t,1,1)..". "
+    end
+      
+    local function getNameString(unitstr)
+        local name = UnitName(unitstr)
+        local size = 15
+        
+        -- first try to only abbreviate the first word
+        if name and strlen(name) > size then
+            name = string.gsub(name, "^(%S+) ", abbrevname)
+        end
+        
+        -- abbreviate all if it still doesn't fit
+        if name and strlen(name) > size then
+            name = string.gsub(name, "(%S+) ", abbrevname)
+        end
+        
+        return name
+    end
+
+    local function abbrevName(frame, unit)
+        local name = getNameString(unit)
+        if name and frame.name then
+            frame.name:SetText(name)
+        end
+    end
+
+    local target = CreateFrame("Frame")
+    target:RegisterEvent("PLAYER_TARGET_CHANGED")
+    target:SetScript("OnEvent", function()
+        abbrevName(TargetFrame, "target")
+    end)
+    
+    local tot = CreateFrame("Frame", nil, TargetFrame)
+    tot:SetScript("OnUpdate", function()
+        abbrevName(TargetofTargetFrame, "targettarget")
+    end)
+end
