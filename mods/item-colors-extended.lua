ac4701a8ea6fec6abb11b88b664bbbcdf3085fec
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-06-12 23:56:26 +0100

    Add files via upload

--- mods/item-colors-extended.lua
+++ mods/item-colors-extended.lua
@@ -12,7 +12,7 @@ local module = ShaguTweaks:register({
 
 local defcolor = {}
 
-suffixes = {
+local suffixes = {
   "of the Tiger", "of the Bear", "of the Gorilla", "of the Boar", "of the Monkey", "of the Falcon", "of the Wolf", "of the Eagle", "of the Whale", "of the Owl", 
   "of Spirit", "of Intellect", "of Strength", "of Stamina", "of Agility", 
   "of Defense", "of Healing", "of Power", "of Blocking", "of Marksmanship", "of Eluding", 
@@ -45,6 +45,11 @@ local function GetItemLinkByName(name)
 end
 
 module.enable = function(self)
+  local dis
+  if IsAddOnLoaded("AdvancedTradeSkillWindow") then
+    dis = true
+  end
+
   do -- merchant
     AddBorder(_G["MerchantBuyBackItemItemButton"], 3, color)
 
@@ -124,48 +129,17 @@ module.enable = function(self)
   end
 
   do -- tradeskill
-    local refresh_tradeskill = function()
-      local id = TradeSkillFrame.selectedSkill
+    if not dis then
+      local refresh_tradeskill = function()
+        local id = TradeSkillFrame.selectedSkill
 
-      do
-        -- icon
-        local button = _G["TradeSkillSkillIcon"]
-        local border = button.ShaguTweaks_border
-
-        if not border then
-          border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
-        end
-
-        if not defcolor["tradeskill"] then
-          defcolor["tradeskill"] = { border:GetBackdropBorderColor() }
-        end
-
-        button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
-
-        local link = GetTradeSkillItemLink(id)
-        if link then
-          local _, _, istring = string.find(link, "|H(.+)|h")
-          local _, _, q = GetItemInfo(istring)
-          if q then
-            local r, g, b = GetItemQualityColor(q)
-            button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
-          end
-        end
-      end
-
-      do
-        -- reagents
-        for i = 1, GetTradeSkillNumReagents(id) do
-          local button = _G["TradeSkillReagent"..i]
+        do
+          -- icon
+          local button = _G["TradeSkillSkillIcon"]
           local border = button.ShaguTweaks_border
 
           if not border then
-            border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
-            border:ClearAllPoints()
-            local icon = _G["TradeSkillReagent"..i.."IconTexture"]
-            border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
-            border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
-            border:SetFrameLevel(1)
+            border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
           end
 
           if not defcolor["tradeskill"] then
@@ -174,79 +148,80 @@ module.enable = function(self)
 
           button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
 
-          local link = GetTradeSkillReagentItemLink(id, i)
+          local link = GetTradeSkillItemLink(id)
           if link then
             local _, _, istring = string.find(link, "|H(.+)|h")
             local _, _, q = GetItemInfo(istring)
-              if q then
+            if q then
               local r, g, b = GetItemQualityColor(q)
               button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
             end
           end
         end
-      end
-    end
 
-    HookAddonOrVariable("Blizzard_TradeSkillUI", function()
-      local HookTradeSkillFrame_Update = TradeSkillFrame_Update
-      TradeSkillFrame_Update = function(arg)
-        HookTradeSkillFrame_Update(arg)
-        refresh_tradeskill()
-      end        
-    end)
+        do
+          -- reagents
+          for i = 1, GetTradeSkillNumReagents(id) do
+            local button = _G["TradeSkillReagent"..i]
+            local border = button.ShaguTweaks_border
+
+            if not border then
+              border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
+              border:ClearAllPoints()
+              local icon = _G["TradeSkillReagent"..i.."IconTexture"]
+              border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
+              border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
+              border:SetFrameLevel(1)
+            end
+
+            if not defcolor["tradeskill"] then
+              defcolor["tradeskill"] = { border:GetBackdropBorderColor() }
+            end
+
+            button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
+
+            local link = GetTradeSkillReagentItemLink(id, i)
+            if link then
+              local _, _, istring = string.find(link, "|H(.+)|h")
+              local _, _, q = GetItemInfo(istring)
+                if q then
+                local r, g, b = GetItemQualityColor(q)
+                button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
+              end
+            end
+          end
+        end
+      end
+
+      HookAddonOrVariable("Blizzard_TradeSkillUI", function()
+        local HookTradeSkillFrame_Update = TradeSkillFrame_Update
+        TradeSkillFrame_Update = function(arg)
+          HookTradeSkillFrame_Update(arg)
+          refresh_tradeskill()
+        end        
+      end)
+    end
   end
 
   do -- craft
-    local refresh_craft = function()
-      local id = GetCraftSelectionIndex()
+    if not dis then
+      local refresh_craft = function()
+        local id = GetCraftSelectionIndex()
 
-      do
-        -- icon
-        local button = _G["CraftIcon"]
-        local border = button.ShaguTweaks_border
-
-        if not border then
-          border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
-        end
-        if not defcolor["craft"] then
-          defcolor["craft"] = { border:GetBackdropBorderColor() }
-        end
-
-        button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
-        local iname = GetCraftInfo(id)
-        local link = GetItemLinkByName(iname)
-        if link then
-          local _, _, istring = string.find(link, "|H(.+)|h")
-          local _, _, q = GetItemInfo(istring)
-          if q then
-            local r, g, b = GetItemQualityColor(q)
-            button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
-          end
-        end
-      end
-
-      do
-        -- reagents
-        for i = 1, GetCraftNumReagents(id) do
-          local button = _G["CraftReagent"..i]
+        do
+          -- icon
+          local button = _G["CraftIcon"]
           local border = button.ShaguTweaks_border
 
           if not border then
-            border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
-            border:ClearAllPoints()
-            local icon = _G["CraftReagent"..i.."IconTexture"]
-            border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
-            border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
-            border:SetFrameLevel(1)
+            border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
           end
-  
           if not defcolor["craft"] then
             defcolor["craft"] = { border:GetBackdropBorderColor() }
           end
 
           button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
-
-          local iname = GetCraftReagentInfo(id, i)
+          local iname = GetCraftInfo(id)
           local link = GetItemLinkByName(iname)
           if link then
             local _, _, istring = string.find(link, "|H(.+)|h")
@@ -257,16 +232,50 @@ module.enable = function(self)
             end
           end
         end
-      end
-    end
 
-    HookAddonOrVariable("Blizzard_CraftUI", function()
-      local HookCraftFrame_Update = CraftFrame_Update
-      CraftFrame_Update = function(arg)
-        HookCraftFrame_Update(arg)
-        refresh_craft()
-      end        
-    end)
+        do
+          -- reagents
+          for i = 1, GetCraftNumReagents(id) do
+            local button = _G["CraftReagent"..i]
+            local border = button.ShaguTweaks_border
+
+            if not border then
+              border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
+              border:ClearAllPoints()
+              local icon = _G["CraftReagent"..i.."IconTexture"]
+              border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
+              border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
+              border:SetFrameLevel(1)
+            end
+    
+            if not defcolor["craft"] then
+              defcolor["craft"] = { border:GetBackdropBorderColor() }
+            end
+
+            button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
+
+            local iname = GetCraftReagentInfo(id, i)
+            local link = GetItemLinkByName(iname)
+            if link then
+              local _, _, istring = string.find(link, "|H(.+)|h")
+              local _, _, q = GetItemInfo(istring)
+              if q then
+                local r, g, b = GetItemQualityColor(q)
+                button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
+              end
+            end
+          end
+        end
+      end
+
+      HookAddonOrVariable("Blizzard_CraftUI", function()
+        local HookCraftFrame_Update = CraftFrame_Update
+        CraftFrame_Update = function(arg)
+          HookCraftFrame_Update(arg)
+          refresh_craft()
+        end        
+      end)
+    end
   end
 
   do -- mail
--- mods/item-colors-glow.lua
+++ mods/item-colors-glow.lua
@@ -64,7 +64,7 @@ local function AddTexture(frame, inset, color)
   return frame.ShaguTweaks_texture
 end
 
-suffixes = {
+local suffixes = {
   "of the Tiger", "of the Bear", "of the Gorilla", "of the Boar", "of the Monkey", "of the Falcon", "of the Wolf", "of the Eagle", "of the Whale", "of the Owl", 
   "of Spirit", "of Intellect", "of Strength", "of Stamina", "of Agility", 
   "of Defense", "of Healing", "of Power", "of Blocking", "of Marksmanship", "of Eluding", 
@@ -97,6 +97,11 @@ local function GetItemLinkByName(name)
 end
 
 module.enable = function(self)
+  local dis
+  if IsAddOnLoaded("AdvancedTradeSkillWindow") then
+    dis = true
+  end
+
   do -- paperdoll
     local refresh_paperdoll = function()
       for i, slot in pairs(paperdoll_slots) do
@@ -390,67 +395,25 @@ module.enable = function(self)
   end
 
   do -- tradeskill
-    local refresh_tradeskill = function()
-      local id = TradeSkillFrame.selectedSkill
+    if not dis then
+      local refresh_tradeskill = function()
+        local id = TradeSkillFrame.selectedSkill
 
-      do
-        -- icon
-        local button = _G["TradeSkillSkillIcon"]
-        local border = button.ShaguTweaks_border
-
-        if not border then
-          border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
-        end
-
-        local texture = button.ShaguTweaks_texture
-
-        if not texture then
-          texture = AddTexture(button, 14, { r = .5, g = .5, b = .5 })
-        end
-
-        if not defcolor["tradeskill"] then
-          defcolor["tradeskill"] = { border:GetBackdropBorderColor() }
-        end
-
-        button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
-        button.ShaguTweaks_texture:SetVertexColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
-
-        local link = GetTradeSkillItemLink(id)
-        if link then
-          local _, _, istring = string.find(link, "|H(.+)|h")
-          local _, _, q = GetItemInfo(istring)
-          if q then
-            local r, g, b = GetItemQualityColor(q)
-            button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
-            button.ShaguTweaks_texture:SetVertexColor(r,g,b,1)
-          end
-        end
-      end
-
-      do
-        -- reagents
-        for i = 1, GetTradeSkillNumReagents(id) do
-          local button = _G["TradeSkillReagent"..i]
+        do
+          -- icon
+          local button = _G["TradeSkillSkillIcon"]
           local border = button.ShaguTweaks_border
 
           if not border then
-            border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
-            border:ClearAllPoints()
-            local icon = _G["TradeSkillReagent"..i.."IconTexture"]
-            border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
-            border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
-            border:SetFrameLevel(1)
+            border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
           end
 
           local texture = button.ShaguTweaks_texture
-  
+
           if not texture then
             texture = AddTexture(button, 14, { r = .5, g = .5, b = .5 })
-            texture:ClearAllPoints()
-            texture:SetPoint("TOPLEFT", border, "TOPLEFT", -12, 12)
-            texture:SetPoint("BOTTOMRIGHT", border, "BOTTOMRIGHT", 12, -12)
           end
-  
+
           if not defcolor["tradeskill"] then
             defcolor["tradeskill"] = { border:GetBackdropBorderColor() }
           end
@@ -458,100 +421,101 @@ module.enable = function(self)
           button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
           button.ShaguTweaks_texture:SetVertexColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
 
-          local link = GetTradeSkillReagentItemLink(id, i)
+          local link = GetTradeSkillItemLink(id)
           if link then
             local _, _, istring = string.find(link, "|H(.+)|h")
             local _, _, q = GetItemInfo(istring)
-              if q then
+            if q then
               local r, g, b = GetItemQualityColor(q)
               button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
               button.ShaguTweaks_texture:SetVertexColor(r,g,b,1)
             end
           end
         end
-      end
-    end
 
-    HookAddonOrVariable("Blizzard_TradeSkillUI", function()
-      local HookTradeSkillFrame_Update = TradeSkillFrame_Update
-      TradeSkillFrame_Update = function(arg)
-        HookTradeSkillFrame_Update(arg)
-        refresh_tradeskill()
-      end        
-    end)
+        do
+          -- reagents
+          for i = 1, GetTradeSkillNumReagents(id) do
+            local button = _G["TradeSkillReagent"..i]
+            local border = button.ShaguTweaks_border
+
+            if not border then
+              border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
+              border:ClearAllPoints()
+              local icon = _G["TradeSkillReagent"..i.."IconTexture"]
+              border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
+              border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
+              border:SetFrameLevel(1)
+            end
+
+            local texture = button.ShaguTweaks_texture
+    
+            if not texture then
+              texture = AddTexture(button, 14, { r = .5, g = .5, b = .5 })
+              texture:ClearAllPoints()
+              texture:SetPoint("TOPLEFT", border, "TOPLEFT", -12, 12)
+              texture:SetPoint("BOTTOMRIGHT", border, "BOTTOMRIGHT", 12, -12)
+            end
+    
+            if not defcolor["tradeskill"] then
+              defcolor["tradeskill"] = { border:GetBackdropBorderColor() }
+            end
+
+            button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
+            button.ShaguTweaks_texture:SetVertexColor(defcolor["tradeskill"][1], defcolor["tradeskill"][2], defcolor["tradeskill"][3], 0)
+
+            local link = GetTradeSkillReagentItemLink(id, i)
+            if link then
+              local _, _, istring = string.find(link, "|H(.+)|h")
+              local _, _, q = GetItemInfo(istring)
+                if q then
+                local r, g, b = GetItemQualityColor(q)
+                button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
+                button.ShaguTweaks_texture:SetVertexColor(r,g,b,1)
+              end
+            end
+          end
+        end
+      end
+
+      HookAddonOrVariable("Blizzard_TradeSkillUI", function()
+        local HookTradeSkillFrame_Update = TradeSkillFrame_Update
+        TradeSkillFrame_Update = function(arg)
+          HookTradeSkillFrame_Update(arg)
+          refresh_tradeskill()
+        end        
+      end)
+    end
   end
 
   do -- craft
-    local refresh_craft = function()
-      local id = GetCraftSelectionIndex()
+    if not dis then
+      local refresh_craft = function()
+        local id = GetCraftSelectionIndex()
 
-      do
-        -- icon
-        local button = _G["CraftIcon"]
-        local border = button.ShaguTweaks_border
-
-        if not border then
-          border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
-        end
-
-        local texture = button.ShaguTweaks_texture
-
-        if not texture then
-          texture = AddTexture(button, 14, { r = .5, g = .5, b = .5 })
-        end
-
-        if not defcolor["craft"] then
-          defcolor["craft"] = { border:GetBackdropBorderColor() }
-        end
-
-        button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
-        button.ShaguTweaks_texture:SetVertexColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
-        
-        local iname = GetCraftInfo(id)
-        local link = GetItemLinkByName(iname)
-        if link then
-          local _, _, istring = string.find(link, "|H(.+)|h")
-          local _, _, q = GetItemInfo(istring)
-          if q then
-            local r, g, b = GetItemQualityColor(q)
-            button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
-            button.ShaguTweaks_texture:SetVertexColor(r,g,b,1)
-          end
-        end
-      end
-
-      do
-        -- reagents
-        for i = 1, GetCraftNumReagents(id) do
-          local button = _G["CraftReagent"..i]
+        do
+          -- icon
+          local button = _G["CraftIcon"]
           local border = button.ShaguTweaks_border
 
           if not border then
-            border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
-            border:ClearAllPoints()
-            local icon = _G["CraftReagent"..i.."IconTexture"]
-            border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
-            border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
-            border:SetFrameLevel(1)
+            border = AddBorder(button, 3, { r = .5, g = .5, b = .5 })
           end
-            
+
           local texture = button.ShaguTweaks_texture
-  
+
           if not texture then
             texture = AddTexture(button, 14, { r = .5, g = .5, b = .5 })
-            texture:ClearAllPoints()
-            texture:SetPoint("TOPLEFT", border, "TOPLEFT", -12, 12)
-            texture:SetPoint("BOTTOMRIGHT", border, "BOTTOMRIGHT", 12, -12)
           end
-  
+
           if not defcolor["craft"] then
             defcolor["craft"] = { border:GetBackdropBorderColor() }
           end
 
           button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
           button.ShaguTweaks_texture:SetVertexColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
-
-          local iname = GetCraftReagentInfo(id, i)
+          
+          local iname = GetCraftInfo(id)
           local link = GetItemLinkByName(iname)
           if link then
             local _, _, istring = string.find(link, "|H(.+)|h")
@@ -563,16 +527,61 @@ module.enable = function(self)
             end
           end
         end
-      end
-    end
 
-    HookAddonOrVariable("Blizzard_CraftUI", function()
-      local HookCraftFrame_Update = CraftFrame_Update
-      CraftFrame_Update = function(arg)
-        HookCraftFrame_Update(arg)
-        refresh_craft()
-      end        
-    end)
+        do
+          -- reagents
+          for i = 1, GetCraftNumReagents(id) do
+            local button = _G["CraftReagent"..i]
+            local border = button.ShaguTweaks_border
+
+            if not border then
+              border = AddBorder(button, 1, { r = .5, g = .5, b = .5 })
+              border:ClearAllPoints()
+              local icon = _G["CraftReagent"..i.."IconTexture"]
+              border:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
+              border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
+              border:SetFrameLevel(1)
+            end
+              
+            local texture = button.ShaguTweaks_texture
+    
+            if not texture then
+              texture = AddTexture(button, 14, { r = .5, g = .5, b = .5 })
+              texture:ClearAllPoints()
+              texture:SetPoint("TOPLEFT", border, "TOPLEFT", -12, 12)
+              texture:SetPoint("BOTTOMRIGHT", border, "BOTTOMRIGHT", 12, -12)
+            end
+    
+            if not defcolor["craft"] then
+              defcolor["craft"] = { border:GetBackdropBorderColor() }
+            end
+
+            button.ShaguTweaks_border:SetBackdropBorderColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
+            button.ShaguTweaks_texture:SetVertexColor(defcolor["craft"][1], defcolor["craft"][2], defcolor["craft"][3], 0)
+
+            local iname = GetCraftReagentInfo(id, i)
+            local link = GetItemLinkByName(iname)
+            if link then
+              local _, _, istring = string.find(link, "|H(.+)|h")
+              local _, _, q = GetItemInfo(istring)
+              if q then
+                local r, g, b = GetItemQualityColor(q)
+                button.ShaguTweaks_border:SetBackdropBorderColor(r,g,b,1)
+                button.ShaguTweaks_texture:SetVertexColor(r,g,b,1)
+              end
+            end
+          end
+        end
+      end
+
+      HookAddonOrVariable("Blizzard_CraftUI", function()
+        local HookCraftFrame_Update = CraftFrame_Update
+        CraftFrame_Update = function(arg)
+          HookCraftFrame_Update(arg)
+          refresh_craft()
+        end        
+      end)
+    end
   end
 
   do -- mail
