4621057a6ba8ac8649f12c0f4b4e983f007c7847
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-11-03 00:15:23 +0000

    Add files via upload

---
+++ mods/actionbar-front-gryphons.lua
@@ -0,0 +1,62 @@
+local _G = ShaguTweaks.GetGlobalEnv()
+
+local module = ShaguTweaks:register({
+    title = "Gryphons in Front",
+    description = "Puts the gryphons in front of the action buttons.",
+    expansions = { ["vanilla"] = true, ["tbc"] = nil },
+    category = "Action Bar",
+    enabled = nil,
+})
+
+module.enable = function(self)
+    local function gryphons()
+        local bars = { MultiBarBottomLeft, MultiBarBottomRight }
+
+        for _, bar in pairs(bars) do
+            bar:SetFrameStrata("LOW")
+
+            for i = 1, 12 do
+                for _, button in pairs(
+                        {
+                        _G[bar:GetName()..'Button'..i],
+                    }
+                ) do
+                    if button.mouseover then
+                        local level = button:GetFrameLevel()
+                        button.mouseover:SetFrameStrata("LOW")
+                        button.mouseover:SetFrameLevel(level+1)
+                    end
+                end
+            end
+            
+            if bar.mouseover then
+                local level = bar:GetFrameLevel()
+                bar.mouseover:SetFrameStrata("LOW")
+                bar.mouseover:SetFrameLevel(level+1)
+            end
+        end
+    end
+
+    local timer = CreateFrame("Frame")
+    timer:Hide()
+    timer:SetScript("OnUpdate", function()
+        if GetTime() >= timer.time then
+            timer.time = nil
+            gryphons()
+            this:Hide()
+            this:SetScript("OnUpdate", nil)
+        end
+    end)
+
+    local events = CreateFrame("Frame", nil, UIParent)
+    events:RegisterEvent("PLAYER_ENTERING_WORLD")
+    events:RegisterEvent("CVAR_UPDATE")
+
+    events:SetScript("OnEvent", function()
+        if not this.loaded then
+            this.loaded = true
+            timer.time = GetTime() + .5
+        timer:Show()
+        end
+    end)
+end
