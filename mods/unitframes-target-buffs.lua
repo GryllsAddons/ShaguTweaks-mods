aab66c9b1d7f31e1f2916cbfdf841875254b80db
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-11-04 22:34:58 +0000

    Add files via upload

--- mods/unitframes-target-buffs.lua
+++ mods/unitframes-target-buffs.lua
@@ -7,67 +7,82 @@ local module = ShaguTweaks:register({
 })
 
 module.enable = function(self)
+	-- Note: The default target frame shows 5 buffs and 16 debuffs
 	local buffButtons = 16
+	local debuffButtons = 16
 
-	do -- create additional buff buttons		
-		for i = 6, buffButtons do		
+	do -- create 11 additional buff buttons		
+		for i = 6, buffButtons do
 			local button = CreateFrame("Button", "TargetFrameBuff"..i, TargetFrame, "TargetBuffButtonTemplate")
 			button:SetID(i)
-			if (i == 6) then
-				button:SetPoint("TOPLEFT", TargetFrameBuff1, "BOTTOMLEFT", 0, -2)
-			else
-				button:SetPoint("LEFT", "TargetFrameBuff"..i-1, "RIGHT", 3, 0)
-			end
 		end
 	end
 
-	local function TargetFrameBuff_Anchor(num_buffs)
-		if (num_buffs > 5) then
-			return 6 -- anchor to third line
+	local function layout(frame, numButtons, rowLimit)
+		-- layout buffs in rows attached to the first buff/debuff
+		local rowBuff = 1 -- the first buff on the last row of buffs/debuffs
+		local added = 1 -- we have already got one buff in the row when we start
+		for i = 2, numButtons do
+			local button = getglobal(frame..i)
+			button:ClearAllPoints()
+
+			if added < rowLimit then -- continue the row
+				button:SetPoint("LEFT", getglobal(frame..i-1), "RIGHT", 3, 0)
+				added = added + 1
+			else -- start a new row
+				button:SetPoint("TOPLEFT", getglobal(frame..i-(rowLimit)), "BOTTOMLEFT", 0, -3)
+				rowBuff = i
+				added = 1
+			end
 		end
-		return 1 -- anchor to second line
+
+		return rowBuff
+	end
+
+	local function layoutFriendly(num_buff, num_debuff, rowLimit)
+		if num_buff > 0 then
+			local rowBuff = layout("TargetFrameBuff", num_buff, rowLimit) -- layout buffs
+			TargetFrameDebuff1:SetPoint("TOPLEFT", "TargetFrameBuff"..rowBuff, "BOTTOMLEFT", 0, -3) -- set debuff anchor
+		else
+			TargetFrameDebuff1:SetPoint("TOPLEFT", "TargetFrame", "BOTTOMLEFT", 5, 32)
+		end
+		layout("TargetFrameDebuff", num_debuff, rowLimit) -- layout debuffs
+	end
+
+	local function layoutHostile(num_buff, num_debuff, rowLimit)
+		if num_debuff > 0 then
+			local rowBuff = layout("TargetFrameDebuff", num_debuff, rowLimit) -- layout debuffs
+			TargetFrameBuff1:SetPoint("TOPLEFT", "TargetFrameDebuff"..rowBuff, "BOTTOMLEFT", 0, -3) -- set buff anchor
+		else
+			TargetFrameBuff1:SetPoint("TOPLEFT", "TargetFrame", "BOTTOMLEFT", 5, 32)
+		end
+		layout("TargetFrameBuff", num_buff, rowLimit) -- layout buffs
 	end
 
 	local function TargetFrameBuff_Position(num_buff, num_debuff)
 		-- position buffs/debuffs depending on whether the targeted unit is friendly or not
 		if (UnitIsFriend("player", "target")) then
-			-- unit is friendly
+			-- unit is friendly, show buffs first then debuffs
 			TargetFrameBuff1:ClearAllPoints()
-			TargetFrameBuff1:SetPoint("TOPLEFT", "TargetFrame", "BOTTOMLEFT", 5, 32)
-			
-			TargetFrameDebuff1:ClearAllPoints()
-			TargetFrameDebuff1:SetPoint("TOPLEFT", "TargetFrameBuff"..TargetFrameBuff_Anchor(num_buff), "BOTTOMLEFT", 0, -2)
+			TargetFrameBuff1:SetPoint("TOPLEFT", "TargetFrame", "BOTTOMLEFT", 5, 32) -- set position of first buff	
+			if not UnitExists("targettarget") then
+				-- show rows of 5 buffs/debuffs if no target of target
+				layoutFriendly(num_buff, num_debuff, 5)
+			else
+				-- show rows of 4 buffs/debuffs if target of target
+				layoutFriendly(num_buff, num_debuff, 4)
+			end
 		else
-			-- unit is hostile
+			-- unit is hostile, show debuffs first then buffs
 			TargetFrameDebuff1:ClearAllPoints()
-			TargetFrameDebuff1:SetPoint("TOPLEFT", "TargetFrame", "BOTTOMLEFT", 5, 32)
-
-			TargetFrameBuff1:ClearAllPoints()
-			TargetFrameBuff1:SetPoint("TOPLEFT", "TargetFrameDebuff"..TargetFrameBuff_Anchor(num_debuff), "BOTTOMLEFT", 0, -2)
-		end
-
-		if not UnitExists("targettarget") then
-			-- show rows of 5 buffs/debuffs if no target of target
-			TargetFrameBuff5:ClearAllPoints()
-			TargetFrameBuff5:SetPoint("LEFT", "TargetFrameBuff4", "RIGHT", 3, 0)
-			TargetFrameBuff10:ClearAllPoints()
-			TargetFrameBuff10:SetPoint("LEFT", "TargetFrameBuff9", "RIGHT", 3, 0)
-
-			TargetFrameDebuff5:ClearAllPoints()
-			TargetFrameDebuff5:SetPoint("LEFT", "TargetFrameDebuff4", "RIGHT", 3, 0)				
-			TargetFrameDebuff10:ClearAllPoints()
-			TargetFrameDebuff10:SetPoint("LEFT", "TargetFrameDebuff9", "RIGHT", 3, 0)
-		else
-			-- show rows of 4 buffs/debuffs if target of target
-			TargetFrameBuff5:ClearAllPoints()
-			TargetFrameBuff5:SetPoint("TOPLEFT", "TargetFrameBuff1", "BOTTOMLEFT", 0, -2)				
-			TargetFrameBuff10:ClearAllPoints()
-			TargetFrameBuff10:SetPoint("TOPLEFT", "TargetFrameBuff5", "BOTTOMLEFT", 0, -2)
-			
-			TargetFrameDebuff5:ClearAllPoints()
-			TargetFrameDebuff5:SetPoint("TOPLEFT", "TargetFrameDebuff1", "BOTTOMLEFT", 0, -2)				
-			TargetFrameDebuff10:ClearAllPoints()
-			TargetFrameDebuff10:SetPoint("TOPLEFT", "TargetFrameDebuff5", "BOTTOMLEFT", 0, -2)
+			TargetFrameDebuff1:SetPoint("TOPLEFT", "TargetFrame", "BOTTOMLEFT", 5, 32) -- set position of first debuff
+			if not UnitExists("targettarget") then
+				-- show rows of 5 buffs/debuffs if no target of target
+				layoutHostile(num_buff, num_debuff, 5)
+			else
+				-- show rows of 4 buffs/debuffs if target of target
+				layoutHostile(num_buff, num_debuff, 4)
+			end
 		end
 	end
 	
@@ -93,7 +108,7 @@ module.enable = function(self)
 	
 		-- debuffs
 		local debuff, debuffApplication, debuffCount
-		for i=1, buffButtons do
+		for i=1, debuffButtons do
 			debuff, debuffApplications = UnitDebuff("target", i)
 			button = getglobal("TargetFrameDebuff"..i)
 			if (debuff) then
